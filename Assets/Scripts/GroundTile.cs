84883677cd43237a13ca168643f820b8a2145a39
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-02-23 13:08:42 -0400

    Adapt GroundTile to handle Obstacles

--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -5,14 +5,56 @@ using UnityEngine;
 public class GroundTile : MonoBehaviour
 {
 	GroundSpawner groundSpawner;
+	int tracks;
+	public double chanceOfObstacle = 0.5;
+
 	void Start()
 	{
 		groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+
+		tracks = (int)transform.localScale.x;
+
+		for (int i = 0; i < tracks; i++)
+		{
+			SpawnObstacle(i);
+		}
 	}
 
-	void OnTriggerExit(Collider other)
+	void OnCollisionExit(Collision other)
 	{
-		groundSpawner.SpawnTile();
-		Destroy(gameObject, 2);
+		if (other.gameObject.tag == "Player")
+		{
+			groundSpawner.SpawnTile();
+			Destroy(gameObject, 2);
+		}
+	}
+
+	public GameObject obstaclePrefab;
+
+	void SpawnObstacle(int obstacleTrack)
+	{
+		if (Random.value >= chanceOfObstacle)
+		{
+			return;
+		}
+
+		// Planes have size 10
+		float trackSize = 10F / (float)tracks;
+		// Choose random track
+
+		// Then, -5 is the border of a plane, and center the obstacle in the "track"
+		Vector3 spawnPosition = new Vector3(-5F + trackSize / 2F + obstacleTrack * trackSize, 0.4F, 5F);
+
+		// Spawn the obstacle
+		// Make this the parent of the obstacle, so it gets destroyed with it
+		GameObject obstacle = Instantiate(obstaclePrefab, transform, false);
+		obstacle.transform.localPosition = spawnPosition;
+
+		// HACK: Fix scale of obstacle to make it 1x1x1 again
+		Vector3 obstacleScale = obstacle.transform.localScale;
+		obstacleScale.x /= transform.localScale.x;
+		obstacleScale.y /= transform.localScale.y;
+		obstacleScale.z /= transform.localScale.z;
+		obstacle.transform.localScale = obstacleScale;
 	}
 }
