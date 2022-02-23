c9273a9430005d9a8aa4f0abee0ccbaa1e0cc278
Michael Woo <michaelwoov@gmail.com>
2022-02-23 21:26:27 +0100

    Added Obstacles 3D models. Added support for spawning more than 1 type of obstacle

--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class GroundTile : MonoBehaviour
 {
+	[SerializeField] private List<GameObject> obstaclePrefabs;
+	[SerializeField] private double chanceOfObstacle = 0.5;
+
 	GroundSpawner groundSpawner;
 	int tracks;
-	public double chanceOfObstacle = 0.5;
 
 	void Start()
 	{
@@ -29,8 +31,6 @@ public class GroundTile : MonoBehaviour
 		}
 	}
 
-	public GameObject obstaclePrefab;
-
 	void SpawnObstacle(int obstacleTrack)
 	{
 		if (Random.value >= chanceOfObstacle)
@@ -43,11 +43,11 @@ public class GroundTile : MonoBehaviour
 		// Choose random track
 
 		// Then, -5 is the border of a plane, and center the obstacle in the "track"
-		Vector3 spawnPosition = new Vector3(-5F + trackSize / 2F + obstacleTrack * trackSize, 0.4F, 5F);
+		Vector3 spawnPosition = new Vector3(-5F + trackSize / 2F + obstacleTrack * trackSize, 0F, 5F);
 
 		// Spawn the obstacle
 		// Make this the parent of the obstacle, so it gets destroyed with it
-		GameObject obstacle = Instantiate(obstaclePrefab, transform, false);
+		GameObject obstacle = Instantiate(getRandomObstaclePrefab(), transform, false);
 		obstacle.transform.localPosition = spawnPosition;
 
 		// HACK: Fix scale of obstacle to make it 1x1x1 again
@@ -57,4 +57,8 @@ public class GroundTile : MonoBehaviour
 		obstacleScale.z /= transform.localScale.z;
 		obstacle.transform.localScale = obstacleScale;
 	}
+
+	private GameObject getRandomObstaclePrefab() {
+		return obstaclePrefabs[Random.Range(0, obstaclePrefabs.Count)];
+    }
 }
