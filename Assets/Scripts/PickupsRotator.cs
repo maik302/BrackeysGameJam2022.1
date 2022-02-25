8f117f0148837f6c0f7eb3e907e6e43914030ffe
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-02-25 18:39:50 -0400

    Small animaiton for coins when picked up

--- Assets/Scripts/PickupsRotator.cs
+++ Assets/Scripts/PickupsRotator.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class PickupsRotator : MonoBehaviour
 {
     [SerializeField] private float _rotationSpeed = 1f;
+    [SerializeField] private float _upwardsWhenEnding = 10f;
+    private float _upwardsSpeed = 0;
 
     void Update()
     {
         transform.Rotate(Vector3.up * _rotationSpeed * Time.deltaTime);
+        transform.Translate(Vector3.up * _upwardsSpeed * Time.deltaTime);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // TODO: Check if its player
+        GameManager.inst.AddPoints(1);
+
+        // Destruction animation
+        _rotationSpeed *= 100;
+        _upwardsSpeed = _upwardsWhenEnding;
+
+        Destroy(gameObject, 0.5f);
+    }
+
 }
