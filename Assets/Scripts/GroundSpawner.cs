359fb70f4f09c7e4f1da58826305da5dba25997c
Michael Woo <michaelwoov@gmail.com>
2022-02-26 19:49:38 +0100

    Updated `GroundSpawner` to manage when to spawn a GroundTile or a CloningTile

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -5,23 +5,55 @@ using UnityEngine;
 public class GroundSpawner : MonoBehaviour {
 	public static GroundSpawner Instance;
 
+	[Header("Ground tiles")]
 	[SerializeField] private GameObject _groundTilePrefab;
-	Vector3 nextSpawnPoint = Vector3.zero;
+	[SerializeField] private int _minGroundTiles;
+	[SerializeField] private int _maxGroundTiles;
+
+	[Header("Cloning tiles")]
+	[SerializeField] private GameObject _cloningTilePrefab;
+
+	Vector3 _nextSpawnPoint = Vector3.zero;
+
+	int _nextNumberOfGroundTilesToSpawn;
+	int _spawnedGroundTiles;
 
     void Awake() {
 		if (Instance == null) {
 			Instance = this;
         }
+
+		ResetGroundTilesToSpawn();
     }
 
-    public void SpawnTile()
-	{
-		GameObject spawned = Instantiate(_groundTilePrefab, nextSpawnPoint, Quaternion.identity, transform);
-		nextSpawnPoint = spawned.transform.Find("Next Tile").transform.position;
+	void ResetGroundTilesToSpawn() {
+		_nextNumberOfGroundTilesToSpawn = Random.Range(_minGroundTiles, _maxGroundTiles);
+		_spawnedGroundTiles = 0;
 	}
 
-	void Start()
-	{
+    public void SpawnTile() {
+		if (_spawnedGroundTiles <= _nextNumberOfGroundTilesToSpawn) {
+			SpawnGroundTile();
+        } else {
+			ResetGroundTilesToSpawn();
+			SpawnCloningTile();
+        }
+	}
+
+	GameObject SpawnGroundTile() {
+		GameObject spawned = Instantiate(_groundTilePrefab, _nextSpawnPoint, Quaternion.identity, transform);
+		_nextSpawnPoint = spawned.transform.Find("Next Tile").transform.position;
+		_spawnedGroundTiles++;
+		return spawned;
+	}
+
+	GameObject SpawnCloningTile() {
+		GameObject spawned = Instantiate(_cloningTilePrefab, _nextSpawnPoint, Quaternion.identity, transform);
+		_nextSpawnPoint = spawned.transform.Find("Next Tile").transform.position;
+		return spawned;
+	}
+
+	void Start() {
 		for (int i = 0; i < 10; i++)
 		{
 			SpawnTile();
