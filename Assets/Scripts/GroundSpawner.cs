b2469fb03e5dfcbe35200222287fb66cb32c38ba
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-02-26 14:46:17 -0400

    Make a hit to player remove all clones, if there are none, take the hit points

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -3,29 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundSpawner : MonoBehaviour {
-	public static GroundSpawner Instance;
+    public static GroundSpawner Instance;
 
-	[SerializeField] private GameObject _groundTilePrefab;
-	Vector3 nextSpawnPoint = Vector3.zero;
+    [SerializeField] private GameObject _groundTilePrefab;
+    [SerializeField] private GameObject _cloneTilePrefab;
+    Vector3 nextSpawnPoint = Vector3.zero;
 
     void Awake() {
-		if (Instance == null) {
-			Instance = this;
+        if (Instance == null) {
+            Instance = this;
         }
     }
 
     public void SpawnTile()
-	{
-		GameObject spawned = Instantiate(_groundTilePrefab, nextSpawnPoint, Quaternion.identity, transform);
-		nextSpawnPoint = spawned.transform.Find("Next Tile").transform.position;
-	}
+    {
+        var prefab = _groundTilePrefab;
+        if (Random.value <= 0.4) {
+            prefab = _cloneTilePrefab;
+        }
+        GameObject spawned = Instantiate(prefab, nextSpawnPoint, Quaternion.identity, transform);
+        nextSpawnPoint = spawned.transform.Find("Next Tile").transform.position;
+    }
 
-	void Start()
-	{
-		for (int i = 0; i < 10; i++)
-		{
-			SpawnTile();
-		}
-	}
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            SpawnTile();
+        }
+    }
 
 }
